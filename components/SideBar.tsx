569b4c69d3125bc388bd30f4ab7307d6379e9cb5
Md. Sindabad <pavel.sindabad@gmail.com>
2022-01-29 19:32:21 +0600

    home main section completed

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -2,7 +2,7 @@
 const SideBar = () => {
     return <div className='md:col-span-1 bg-yellow-200 my-4 md:my-0'>
         <h1>Sidebar</h1>
-        <p>lorem200</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At eveniet modi dolorem facere, nisi quidem tenetur vero iusto corporis beatae, harum inventore ullam debitis doloribus natus voluptates! Reprehenderit soluta culpa dolores fugiat doloremque consequuntur, odio aspernatur voluptates alias! Iure cupiditate, nesciunt, ea at cum explicabo inventore maiores incidunt corrupti quasi pariatur cumque aut dolores. Perspiciatis, cupiditate! Deserunt at dolorem esse magnam voluptas quas, quidem consectetur? Eos, nesciunt repudiandae est, unde vitae voluptates veniam necessitatibus impedit illum ea rerum animi eum perferendis praesentium fugit enim. Iste similique doloremque deserunt maiores ad quod, modi perspiciatis! Sed sit libero veritatis soluta cupiditate, similique nobis atque, perspiciatis est natus quia laborum exercitationem dolorem excepturi inventore beatae saepe molestias rerum. Esse, voluptatum sequi voluptates praesentium modi omnis illo maiores dolorem minus suscipit a unde velit dignissimos expedita itaque quae aut saepe dolorum in? Eum sapiente iste quas repellendus! Molestiae culpa quibusdam commodi consequuntur numquam optio accusamus beatae id, at ducimus autem harum atque quae cupiditate, ratione iusto. Quaerat perspiciatis ex totam voluptatem nisi. Dolorem aspernatur, corporis impedit quaerat ex quibusdam maiores dicta error hic consequatur eum quae modi mollitia fugit nostrum iste eius cum voluptates voluptas perferendis a! Id recusandae animi ut quos, cupiditate commodi.</p>
     </div>;
 };
 
--- components/TopNews.tsx
+++ components/TopNews.tsx
@@ -7,15 +7,16 @@ const TopNews = ({ news }) => {
     return <div className='md:col-span-3 bg-gray-100 cursor-pointer md:flex flex-wrap px-2 justify-around block'>
         {
             news?.map((article, index) => (
-                <div key={article.title} className={`mt-4 p-1 ${index === 0 ? 'basis-full' : 'basis-5/12 hover:scale-105 transition'}`}>
-                    <Image
+                <div key={article.title} className={`mt-4 p-1 ${index === 0 ? 'basis-full' : 'basis-5/12 border transition hover:shadow hover:shadow-rose-600'}`}>
+                    {/* <Image
                         src={article.urlToImage}
                         alt={article.title}
                         width={index === 0 ? 800 : 400}
                         height={index === 0 ? 400 : 200}
                         className='object-cover'
-                    />
-                    <h1 className={`font-bold text-xl text-gray-700 hover:text-rose-500 transition ${index === 0 && 'text-3xl md:text-4xl'}`}>{article.title}</h1>
+                    /> */}
+                    <img src={article.urlToImage || '/images/placeholder.png'} alt={article.title} />
+                    <h1 className={`line-clamp-3 font-bold text-xl text-gray-700 hover:text-rose-500 transition ${index === 0 && 'text-3xl md:text-4xl'}`}>{article.title}</h1>
                     <div className='my-1'>
                         <div className='flex items-center text-gray-500 mr-2'>
                             <AiOutlineUser />
@@ -27,6 +28,10 @@ const TopNews = ({ news }) => {
                         </div>
                         <div className='flex items-center text-gray-500'></div>
                     </div>
+                    <div>
+                        <p className='line-clamp-3'>{article.content || article.description}</p>
+                    </div>
+                    <a className='font-bold hover:text-rose-600' href={article.url} target='_blank'>Read more...</a>
                     <hr />
                 </div>
             ))
